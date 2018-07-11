041536de4d5350ebb97b0b3be7bde73f08e2761b
Mark Jordan <mjordan@sfu.ca>
2018-07-11 14:15:17 +0000

    Address #1.

--- islandora_change_cmodel.api.php
+++ islandora_change_cmodel.api.php
@@ -5,6 +5,31 @@
  */
 
 /**
+ * Fires just before the content model is changed. Can block the change.
+ *
+ * @param string $pid
+ *   The PID of the object whose content model has just been changed.
+ * @param string $old_cmodel
+ *   The PID of the content model that is being removed.
+ * @param string $new_cmodel
+ *   The PID of the content model that is being added.
+ *
+ * @return bool
+ *   TRUE to not block the change, FALSE to block the change.
+ */
+function mymodule_islandora_change_cmodel_prechange($pid, $old_cmodel, $new_cmodel) {
+  global $user;
+  if ($user->name == 'rusty' && $pid == 'islandora:100') {
+    drupal_set_message("Rusty, I forbid you from changing this node's content model", 'error');
+    return FALSE;
+  }
+  else {
+    // Everyone else is allowed to change the cmodel.
+    return TRUE;
+  }
+}
+
+/**
  * Informs modules that an object's content model has been changed.
  *
  * @param string $pid
